a1ca469d090e53b83a4b6f6b1b3f41c7bc7dc7bb
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2020-12-30 00:23:37 +0700

    Go!

---
+++ app.js
@@ -0,0 +1,52 @@
+const appForm = document.getElementById("num-form");
+const numInput = document.getElementById("num-input");
+const msg = document.getElementById("txt");
+const btnPlace = document.getElementById("play-again");
+const btnSubmit = document.getElementById("num-submit");
+let num;
+let trial = 2;
+
+num = Math.floor(Math.random() * 10) + 1;
+
+appForm.addEventListener("submit", function(e){
+    if(trial > 0){
+        if(num === Number(numInput.value)){
+            msg.textContent = "Congrats!";
+            numInput.disabled = true;
+            btnSubmit.disabled = true;
+
+            showBtnPlayAgain();
+        }else{
+            msg.textContent = "Sorry!, " + trial + " trial left.";
+        }
+    }else{
+        msg.textContent = "Sorry! You failed.";
+        numInput.disabled = true;
+        btnSubmit.disabled = true;
+
+        if(trial === 0){
+            showBtnPlayAgain();
+        }
+    }
+    
+    trial -= 1;
+    e.preventDefault();
+});
+
+function showBtnPlayAgain(){
+    const btnNew = document.createElement("button");
+    btnNew.innerText = "Play Again!";
+    btnNew.id = "btnPlayAgain";
+    btnPlace.appendChild(btnNew);
+
+    btnNew.addEventListener("click", function(){
+        trial = 2;
+        num = Math.floor(Math.random() * 10) + 1;
+        numInput.disabled = false;
+
+        msg.textContent = "Go! Go!";
+
+        btnNew.remove();
+        btnSubmit.disabled = false;
+    });
+}
